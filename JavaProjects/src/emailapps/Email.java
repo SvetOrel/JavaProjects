51ba5f786e54e08089d5da472cb2d86632d73993
Sveta Grossman <sunlightsvett@gmail.com>
2022-04-30 16:51:52 +0300

    Email App

--- JavaProjects/src/emailapps/Email.java
+++ JavaProjects/src/emailapps/Email.java
@@ -1,5 +1,30 @@
 package emailapps;
 
+import java.util.Scanner;
+
 public class Email {
-//
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String alternateEmail;
+	private int mailboxCapacity;
+	
+	public Email(String firstName,String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+		this.department = setDepartment();
+		System.out.println("Department: "+this.department);
+	}
+	
+	private String setDepartment() {
+		System.out.print("Department Codes\nEnter the department\n1 for Sales\n2 for Development\3 for Accounting\n0 for none\nEnter department code:");
+		Scanner in =new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if(depChoice == 1) { return "Sales"; }
+		else if(depChoice == 2) { return "Development"; }
+		else if(depChoice == 3) { return "Accounting"; }
+		else { return ""; }
+	}
 }
--- JavaProjects/src/emailapps/EmailApp.java
+++ JavaProjects/src/emailapps/EmailApp.java
@@ -3,8 +3,7 @@ package emailapps;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Email em1 = new Email("John","Smith");
 
 	}
-
 }
